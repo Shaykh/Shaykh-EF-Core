27d8f13961fa187d45a03a76819f156814c377e2
Diaby Cheick <diabycheick@live.fr>
2019-11-06 16:58:14 +0100

    nitialisation du projet Domain

---
+++ ShaykCoreEF/ShaykhCoreEF.Domain/DataTransferObjects/OrderLineItem.cs
@@ -0,0 +1,8 @@
+namespace ShaykhCoreEF.Domain.DataTransferObjects
+{
+    public class OrderLineItem
+    {
+        public int ProductQuantity { get; set; }
+        public string ProductName { get; set; }
+    }
+}
