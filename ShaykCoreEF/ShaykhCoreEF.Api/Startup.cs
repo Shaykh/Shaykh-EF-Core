a94e26585825184c233c6c106c94e7b812e378d1
Diaby Cheick <diabycheick@live.fr>
2019-11-06 16:50:32 +0100

    Initialisation du projet API

---
+++ ShaykCoreEF/ShaykhCoreEF.Api/Startup.cs
@@ -0,0 +1,57 @@
+using ShaykhCoreEF.DataAccess.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+// Add the System.Data.SqlClient using statement
+// Add the ShaykhCoreEF.DataAccess.Services using statement
+
+namespace ShaykhCoreEF.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add the OrderService DI registration code
+
+            // Add the SqlConnectionStringBuilder code
+
+            // Add the UseSqlServer code
+
+            services.AddControllers();
+            services.AddApplicationInsightsTelemetry();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
