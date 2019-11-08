7ff6d283f8d953df36a73232ab809066602698d0
Diaby Cheick <diabycheick@live.fr>
2019-11-08 12:04:02 +0100

    Ecriture des requêtes CRUD

--- ShaykCoreEF/ShaykhCoreEF.DataAccess/Services/OrderService.cs
+++ ShaykCoreEF/ShaykhCoreEF.DataAccess/Services/OrderService.cs
@@ -5,22 +5,101 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using ShaykhCoreEF.DataAccess.Data;
 using ShaykhCoreEF.Domain.DataTransferObjects;
-// Add the ShaykhCoreEF.Domain.Models using statement
+using ShaykhCoreEF.Domain.Models;
 
 namespace ShaykhCoreEF.DataAccess.Services
 {
     public class OrderService
     {
-        // Add the constructor code
+        private readonly ShaykhCoreEFContext _context;
 
-        // Add the GetAll code
+        public OrderService(ShaykhCoreEFContext context)
+        {
+            _context = context;
+        }
 
-        // Add the GetById code
+        public async Task<List<CustomerOrder>> GetAll()
+        {
+            List<CustomerOrder> orders = await (_context.Orders.AsNoTracking()
+                .OrderByDescending(o => o.OrderPlaced)
+                .Select(o => new CustomerOrder
+                {
+                    OrderId = o.Id,
+                    CustomerName = $"{o.Customer.LastName}, {o.Customer.FirstName}",
+                    OrderFulfilled = o.OrderFulfilled.HasValue ? 
+                        o.OrderFulfilled.Value.ToShortDateString() : string.Empty,
+                    OrderPlaced = o.OrderPlaced.ToShortDateString(),
+                    OrderLineItems = (o.ProductOrders.Select(po => new OrderLineItem
+                    {
+                        ProductQuantity = po.Quantity,
+                        ProductName = po.Product.Name
+                    }))
+                })).ToListAsync();
 
-        // Add the Create code
+            return orders;
+        }
 
-        // Add the SetFulfilled code
+        private IQueryable<Order> GetOrderById(int id) => _context.Orders.AsNoTracking().Where(o => o.Id == id);
 
-        // Add the Delete code
+        public async Task<CustomerOrder> GetById(int id)
+        {
+            CustomerOrder order = await GetOrderById(id)
+                .Select(o => new CustomerOrder
+                {
+                    OrderId = o.Id,
+                    CustomerName = $"{o.Customer.LastName}, {o.Customer.FirstName}",
+                    OrderFulfilled = o.OrderFulfilled.HasValue ? 
+                        o.OrderFulfilled.Value.ToShortDateString() : string.Empty,
+                    OrderPlaced = o.OrderPlaced.ToShortDateString(),
+                    OrderLineItems = (o.ProductOrders.Select(po => new OrderLineItem
+                    {
+                        ProductQuantity = po.Quantity,
+                        ProductName = po.Product.Name
+                    }))
+                }).FirstOrDefaultAsync();
+
+            return order;
+        }
+
+        public async Task<Order> Create(Order newOrder)
+        {
+            newOrder.OrderPlaced = DateTime.UtcNow;
+
+            _context.Orders.Add(newOrder);
+            await _context.SaveChangesAsync();
+
+            return newOrder;
+        }
+
+        public async Task<bool> SetFulfilled(int id)
+        {
+            bool isFulfilled = false;
+            Order order = await GetOrderById(id).FirstOrDefaultAsync();
+
+            if (order != null)
+            {
+                order.OrderFulfilled = DateTime.UtcNow;
+                _context.Entry(order).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                isFulfilled = true;
+            }
+
+            return isFulfilled;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            bool isDeleted = false;
+            Order order = await GetOrderById(id).FirstOrDefaultAsync();
+
+            if (order != null)
+            {
+                _context.Remove(order);
+                await _context.SaveChangesAsync();
+                isDeleted = true;
+            }
+
+            return isDeleted;
+        }
     }
 }
